2d6f5812bbfd3ac0545f0e5bfc24ed3e6f982844
nguyenyn <yen.nhi.nguyen@ulb.be>
2023-12-01 16:51:59 +0100

    Bullet library and 2 bullets collision

---
+++ main.cpp
@@ -0,0 +1,433 @@
+#include<iostream>
+
+//include glad before GLFW to avoid header conflict or define "#define GLFW_INCLUDE_NONE"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "camera.h"
+#include "shader.h"
+#include "object.h"
+
+#include <btBulletCollisionCommon.h>
+#include <btBulletDynamicsCommon.h>
+
+
+const int width = 1000;
+const int height = 1000;
+
+
+GLuint compileShader(std::string shaderCode, GLenum shaderType);
+GLuint compileProgram(GLuint vertexShader, GLuint fragmentShader);
+void processInput(GLFWwindow* window);
+
+
+#ifndef NDEBUG
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	unsigned int id,
+	GLenum severity,
+	GLsizei length,
+	const char* message,
+	const void* userParam)
+{
+	// ignore non-significant error/warning codes
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+	} std::cout << std::endl;
+	std::cout << std::endl;
+}
+#endif
+
+Camera camera(glm::vec3(0.0, 1.0, 10));
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Welcome to the bullet demo " << std::endl;
+	std::cout << " This code is an example of the bullet library \n";
+
+	//Boilerplate
+	//Create the OpenGL context 
+	if (!glfwInit()) {
+		throw std::runtime_error("Failed to initialise GLFW \n");
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifndef NDEBUG
+	//create a debug context to help with Debugging
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+#endif
+
+
+	//Create the window
+	GLFWwindow* window = glfwCreateWindow(width, height, "Solution 02", nullptr, nullptr);
+	if (window == NULL)
+	{
+		glfwTerminate();
+		throw std::runtime_error("Failed to create GLFW window\n");
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//load openGL function
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		throw std::runtime_error("Failed to initialize GLAD");
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+#ifndef NDEBUG
+	int flags;
+	glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+	}
+#endif
+
+	const std::string sourceV = "#version 330 core\n"
+		"in vec3 position; \n"
+		"in vec2 tex_coord; \n"
+		"in vec3 normal; \n"
+		"out vec4 v_col; \n"
+		"out vec2 v_t; \n"
+		"uniform mat4 M; \n"
+		"uniform mat4 V; \n"
+		"uniform mat4 P; \n"
+		" void main(){ \n"
+		"gl_Position = P*V*M*vec4(position, 1);\n"
+		"v_col = vec4(normal*0.5 + 0.5, 1.0);\n"
+		"v_t = tex_coord; \n"
+		"}\n";
+	const std::string sourceF = "#version 330 core\n"
+		"out vec4 FragColor;"
+		"precision mediump float; \n"
+		"in vec4 v_col; \n"
+		"in vec2 v_t; \n"
+		"void main() { \n"
+		"FragColor = v_col*(1.0-v_t.y); \n"
+		"} \n";
+
+
+	Shader shader(sourceV, sourceF);
+
+
+	char path[] = PATH_TO_OBJECTS "/sphere_coarse.obj";
+	Object cube(path);
+	cube.makeObject(shader);
+
+	char pathPlane[] = PATH_TO_OBJECTS "/plane.obj";
+	Object plane(pathPlane);
+	plane.makeObject(shader);
+
+
+
+
+	double prev = 0;
+	int deltaFrame = 0;
+	//fps function
+	auto fps = [&](double now) {
+		double deltaTime = now - prev;
+		deltaFrame++;
+		if (deltaTime > 0.5) {
+			prev = now;
+			const double fpsCount = (double)deltaFrame / deltaTime;
+			deltaFrame = 0;
+			std::cout << "\r FPS: " << fpsCount;
+		}
+		};
+
+
+	//1. Create bullet world 
+
+	///-----initialization_start-----
+	///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
+	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+	///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
+	btBroadphaseInterface* overlappingPairCache = new btDbvtBroadphase();
+	///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+	//Finally create the world
+	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+	//add gravity
+	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
+
+	//2. Create the collisions shapes
+	glm::mat4 model = glm::mat4(1.0);
+	glm::mat4 model2 = glm::mat4(2.0);
+	glm::mat4 modelGround = glm::mat4(1.0);
+	modelGround = glm::scale(modelGround, glm::vec3(10, 1, 10));
+	modelGround = glm::translate(modelGround, glm::vec3(0,0,0));
+
+	btAlignedObjectArray<btCollisionShape*> collisionShapes;
+	//2.a We need one for the ground
+	{
+		btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
+
+		collisionShapes.push_back(groundShape);
+
+		btTransform groundTransform;
+		groundTransform.setIdentity();
+		groundTransform.setOrigin(btVector3(0, -50, 0));
+		//already init the model for ground 
+		
+
+		
+
+		btScalar mass(0.);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			groundShape->calculateLocalInertia(mass, localInertia);
+
+		//using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
+		btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
+		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, localInertia);
+		btRigidBody* body = new btRigidBody(rbInfo);
+
+		//add the body to the dynamics world
+		dynamicsWorld->addRigidBody(body);
+	}
+	//2.b Another one is for the sphere
+	btRigidBody* bodySphere;
+	{
+		//create a dynamic rigidbody
+
+		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
+		btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+		collisionShapes.push_back(colShape);
+
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
+
+		btScalar mass(1.f);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(0, 0, 0);
+		if (isDynamic)
+			colShape->calculateLocalInertia(mass, localInertia);
+
+		startTransform.setOrigin(btVector3(2, 10, 0));
+
+		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+		bodySphere = new btRigidBody(rbInfo);
+
+		dynamicsWorld->addRigidBody(bodySphere);
+	}
+	//3.b Another one for another sphere
+	btRigidBody* bodySphere2;
+	{
+		//create a dynamic rigidbody
+
+		//btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
+		btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+		collisionShapes.push_back(colShape);
+
+		/// Create Dynamic Objects
+		btTransform startTransform;
+		startTransform.setIdentity();
+
+		btScalar mass(1.f);
+
+		//rigidbody is dynamic if and only if mass is non zero, otherwise static
+		bool isDynamic = (mass != 0.f);
+
+		btVector3 localInertia(2, 2, 0);
+		if (isDynamic)
+			colShape->calculateLocalInertia(mass, localInertia);
+
+		startTransform.setOrigin(btVector3(1, 20, 0));
+
+		//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+		btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+		bodySphere2 = new btRigidBody(rbInfo);
+
+		dynamicsWorld->addRigidBody(bodySphere2);
+	}
+	
+
+
+	glm::mat4 view = camera.GetViewMatrix();
+	glm::mat4 perspective = camera.GetProjectionMatrix();
+
+
+	glfwSwapInterval(1);
+	//Rendering
+
+	auto lastFrameTime = glfwGetTime();
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+		
+		glfwPollEvents();
+		
+		double now = glfwGetTime();
+		//3. Ask bullet to do the simulation
+		dynamicsWorld->stepSimulation(now -lastFrameTime, 10);
+
+		//4. Get the model matrice for the sphere from bullet
+
+		btTransform transform;
+		bodySphere->getMotionState()->getWorldTransform(transform);
+		transform.getOpenGLMatrix(glm::value_ptr(model));
+
+		btTransform transform2;
+		bodySphere2->getMotionState()->getWorldTransform(transform2);
+		transform2.getOpenGLMatrix(glm::value_ptr(model2));
+
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+		view = camera.GetViewMatrix();
+		// Use the shader Class to send the uniform
+		shader.use();
+		
+		shader.setMatrix4("M", model);
+		shader.setMatrix4("V", view);
+		shader.setMatrix4("P", perspective);
+
+
+		cube.draw();
+
+		shader.setMatrix4("M", model2);
+		cube.draw();
+
+		shader.setMatrix4("M", modelGround);
+		plane.draw();
+
+		fps(now);
+		lastFrameTime = now;
+		glfwSwapBuffers(window);
+	}
+
+	//5. Clean up 
+	//remove the rigidbodies from the dynamics world and delete them
+	for (int i = dynamicsWorld->getNumCollisionObjects() - 1; i >= 0; i--)
+	{
+		btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
+		btRigidBody* body = btRigidBody::upcast(obj);
+		if (body && body->getMotionState())
+		{
+			delete body->getMotionState();
+		}
+		dynamicsWorld->removeCollisionObject(obj);
+		delete obj;
+	}
+	//delete collision shapes
+	for (int j = 0; j < collisionShapes.size(); j++)
+	{
+		btCollisionShape* shape = collisionShapes[j];
+		collisionShapes[j] = 0;
+		delete shape;
+	}
+	//delete dynamics world
+	delete dynamicsWorld;
+	//delete solver
+	delete solver;
+	//delete broadphase
+	delete overlappingPairCache;
+	//delete dispatcher
+	delete dispatcher;
+	delete collisionConfiguration;
+	//next line is optional: it will be cleared by the destructor when the array goes out of scope
+	collisionShapes.clear();
+
+
+	//clean up ressource
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
+
+
+void processInput(GLFWwindow* window) {
+	// Use the cameras class to change the parameters of the camera
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(LEFT, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(RIGHT, 0.1);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(FORWARD, 0.1);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboardMovement(BACKWARD, 0.1);
+
+	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(1, 0.0, 1);
+	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(-1, 0.0, 1);
+
+	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(0.0, 1.0, 1);
+	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		camera.ProcessKeyboardRotation(0.0, -1.0, 1);
+
+
+}
+
+
+
+
